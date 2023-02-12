700659b585b8ca0cc495b5d6a14739efe0bc5d3d
shaneatism1 <122491340+shaneatism1@users.noreply.github.com>
2023-02-11 21:47:16 -0800

    Add files via upload

--- Shane Goodson MyAdventure/Shane Goodson MyAdventure/ViewController.swift
+++ Shane Goodson MyAdventure/Shane Goodson MyAdventure/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var first = FirstLevel()
     var second = SecondLevel()
     var third = ThirdLevel()
+    var fourth = FourthLevel()
     
     @IBOutlet weak var display: UILabel!
     @IBOutlet weak var button2Text: UIButton!
@@ -24,13 +25,19 @@ class ViewController: UIViewController {
             display.text = second.FixEngines()
             button1Text.setTitle("", for: .normal)
             button2Text.setTitle("", for: .normal)
-            level += 2
+            level += 99
         }
         else if (level == 2){
             display.text = third.BlackHole()
+            button1Text.setTitle("War", for: .normal)
+            button2Text.setTitle("Peace", for: .normal)
+            level += 1
+        }
+        else if (level == 3){
+            display.text = fourth.War()
             button1Text.setTitle("", for: .normal)
             button2Text.setTitle("", for: .normal)
-            level += 1
+            level += 99
         }
     }
     
@@ -45,7 +52,13 @@ class ViewController: UIViewController {
             display.text = third.Avoid()
             button1Text.setTitle("", for: .normal)
             button2Text.setTitle("", for: .normal)
-            level += 1
+            level += 99
+        }
+        else if (level == 3){
+            display.text = fourth.Peace()
+            button1Text.setTitle("", for: .normal)
+            button2Text.setTitle("", for: .normal)
+            level += 99
         }
     }
     
@@ -70,16 +83,25 @@ class SecondLevel {
         return "Ran out of O2...game over"
     }
     func FixO2() -> String{
-        return "Now kareeming towards black hole...what do you do?"
+        return "Now youre kareeming towards a black hole...what do you do?"
     }
 }
 
 class ThirdLevel {
     func BlackHole() -> String{
-        return "you are now inside a tesseract...you won"
+        return "you are now inside a tesseract and see aliens...what do you do?"
     }
     func Avoid() -> String{
         return "you ejected into space and are doomed...game over"
+    }
+}
+
+class FourthLevel {
+    func War() -> String{
+        return "you decided to fight the aliens and you are now ruler of the universe...you won"
+    }
+    func Peace() -> String{
+        return "the aliens rejected your peace offer and threw you into the void of outer space...game over"
     }
 }
 
